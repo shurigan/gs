822f10cf688bfa30ce99f814d3616d7b67fd4a99
mashchinskiy <mashchinskiy@playform.com>
2014-02-14 15:35:40 +0400

    sandbox

--- cs/app.js
+++ cs/app.js
@@ -14,10 +14,32 @@ io = io.listen(http);
 
 http.listen(httpPort);
 
+var gs = (function () {
+    return {
+        say: function(player, what) {
+            console.log(what);
+            io.sockets.emit('message', { player: player, what: what })
+        },
+
+    }
+})()
+
 io.sockets.on('connection', function (socket) {
 //    socket.emit('news', { hello: 'world' });
-    socket.on('click', function (data) {
-        io.sockets.emit('news', { hello: data.data });
+    socket.on('action', function (data) {
+        if(data.action == 'click') {
+
+        }
+
         console.log(data);
     });
 });
+
+
+
+var cycle = function() {
+    gs.say(100, "WTF")
+    setTimeout(cycle, 1000);
+};
+
+setTimeout(cycle, 0);
